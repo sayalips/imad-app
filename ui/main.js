15884be1f8152f2d96791a0524dc616f25230c6b
sayalips <sayali1696@gmail.com>
2017-11-17 11:51:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,13 @@ var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+ 'px';
+}
 img.onclick=function() {
-   
-    img.style.marginleft='208px';
-    //alert('hey hiiiii');
+   var interval=setInterval(moveRight,100);
+    //img.style.marginLeft='208px';
+
 };
