f94f985bda3623f3f726989157a8c0da8958f58e
sayalips <sayali1696@gmail.com>
2017-11-17 18:32:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-var name=[];
+var names=[];
 app.get('/submit-name/:name', function (req, res) {
     //get the name from req object
     var name=req.params.name;
