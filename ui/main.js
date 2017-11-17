ad82cb4ef2baac1d63bed6e6a4f7b1995167da6f
sayalips <sayali1696@gmail.com>
2017-11-17 14:13:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,23 @@
 //counter code
 //var counter=0;
 var button=document.getElementById("counter");
+
 button.onclick=function(){
-    counter=counter+1;
-    var spam=document.getElementById('count');
+    
+    //create req
+    var request=new XMLHttpRequest();
+    //capture the response and store it in var
+    request.onreadystatechange=function();
+    if(request.readyState==XMLHttpRequest.DONE)
+    {
+        //action
+        if(requset.status==200){
+            var counter=rquest.responseText;
+              var spam=document.getElementById('count');
     spam.innerHTML=counter.toString();
+        }
+    }
+    //make req
+    request.open('GET','http://sayali1696.imad.hasura.app.io/counter',true);
+    request.send(null);
 };
