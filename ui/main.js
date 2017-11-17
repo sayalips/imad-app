5123768733152f7b8706d8acbdfa9789b1b93611
sayalips <sayali1696@gmail.com>
2017-11-17 13:28:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
 //counter code
+var counter=0;
 var button=document.getElementById("counter");
 button.onclick=function(){
     counter=counter+1;
-    var spam=document.getElementById("count");
+    var spam=document.getElementById('count');
     spam.innerHTML=counter.toString();
 };
